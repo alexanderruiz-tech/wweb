32f1d8a41545b3bc933400b87cfe4fb4095ebf1d
alexanderruiz-tech <alexanderruizorellano@gmail.com>
2024-11-08 17:21:47 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Enlaces de redes sociales
+document.getElementById('facebook').href = 'https://www.facebook.com/unique-drink';
+document.getElementById('instagram').href = 'https://www.instagram.com/unique-drink';
+
+// Reemplaza 'unique-drink' con los enlaces reales de tus redes sociales
